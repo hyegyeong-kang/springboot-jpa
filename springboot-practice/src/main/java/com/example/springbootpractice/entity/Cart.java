13e71612bdf45f6cc9d1db796ed797f40fd8e5cb
hyegyeong-kang <rkdgprud96@gmail.com>
2023-06-18 23:15:26 +0900

    add Cart Entity

---
+++ springboot-practice/src/main/java/com/example/springbootpractice/entity/Cart.java
@@ -0,0 +1,33 @@
+package com.example.springbootpractice.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.Table;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cart")
+@Getter
+@Setter
+@ToString
+public class Cart {
+
+    @Id
+    @Column(name = "cart_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    public static Cart createCart(Member member){
+        Cart cart = new Cart();
+        cart.setMember(member);
+        return cart;
+    }
+}
