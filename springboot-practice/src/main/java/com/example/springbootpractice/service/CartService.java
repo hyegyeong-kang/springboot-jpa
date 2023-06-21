4f658d8c54632e5e713cf25f737082984302a6d4
hyegyeong-kang <rkdgprud96@gmail.com>
2023-06-21 23:52:47 +0900

    add cart service

---
+++ springboot-practice/src/main/java/com/example/springbootpractice/service/CartService.java
@@ -0,0 +1,34 @@
+package com.example.springbootpractice.service;
+
+import com.shop.repository.CartItemRepository;
+import com.shop.repository.CartRepository;
+import com.shop.repository.ItemRepository;
+import com.shop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class CartService {
+
+    private final ItemRepository itemRepository;
+    private final MemberRepository memberRepository;
+    private final CartRepository cartRepository;
+    private final CartItemRepository cartItemRepository;
+
+    public Long addCart(CartItemDto cartItemDto, String email) {
+
+        Item item = itemRepository.findById(cartItemDto.getItemId())
+                .orElseThrow(EntityNotFoundException::new);
+        Member member = memberRepository.findByEmail(email);
+
+        Cart cart = cartRepository.findByMemberId(member.getId());
+        if (cart == null) {
+            cart = Cart.createCart(member);
+            cartRepository.sav
