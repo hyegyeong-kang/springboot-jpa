03927ed1f8f76f2a239d5d5e8a9aada655c8849d
hyegyeong-kang <rkdgprud96@gmail.com>
2023-06-27 10:48:31 +0900

    add Constant files
    ItemSellStatus, OrderStatus, Role

---
+++ springboot-practice/src/main/java/com/example/springbootpractice/constant/ItemSellStatus.java
@@ -0,0 +1,7 @@
+package com.example.springbootpractice.constant;
+
+public enum ItemSellStatus {
+    SELL, SOLD_OUT
+}
+
+
---
+++ springboot-practice/src/main/java/com/example/springbootpractice/constant/Role.java
@@ -0,0 +1,5 @@
+package com.example.springbootpractice.constant;
+
+public enum Role {
+    USER, ADMIN
+}
